1396ddc1f2ca22215af503d49fbdd7db8dfa1826
seven320 <yosyuaomenw@yahoo.co.jp>
2020-06-12 01:02:36 +0900

    test commit

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -309,6 +309,7 @@ func (r *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Con
 
 var db *sql.DB
 
+// ここから起動
 func main() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASS"),
